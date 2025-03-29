1a903f930503ce75a517a8906251138aa6ff3568
Korzhyk Roman <korzhykroman11@gmail.com>
2025-03-29 20:04:25 +0200

    wwww

--- lesson_4.py
+++ lesson_4.py
@@ -1,33 +1,66 @@
-
 class Human:
 
     height = 170
     gladness = 0
 
+    def __init__(self, name):
+        self.name = name
+        self.pet = None  # Посилання на домашню тварину
+
     def printer(self):
-        print('hello im human')
+        print(f'Hello, I am {self.name}, and I am a human.')
+
+    def adopt_pet(self, pet):
+        self.pet = pet
+        print(f'{self.name} adopted {pet.name}!')
+
+    def play_with_pet(self):
+        if self.pet:
+            self.gladness += 10
+            print(f'{self.name} is playing with {self.pet.name}. Gladness increased to {self.gladness}!')
+            self.pet.play()
+        else:
+            print(f'{self.name} has no pet to play with.')
 
 
 class Student(Human):
     gladness = 30
 
     def printer(self):
-        print('hello im student')
+        print(f'Hello, I am {self.name}, and I am a student.')
 
 
 class Worker(Human):
     gladness = 70
+
     def printer(self):
-        print('hello im worker')
+        print(f'Hello, I am {self.name}, and I am a worker.')
 
 
-nick = Worker()
-kate = Student()
+class Pet:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.happiness = 50
+
+    def play(self):
+        self.happiness += 5
+        print(f'{self.name} the {self.species} is happy! Happiness level: {self.happiness}')
+
+
+nick = Worker("Nick")
+kate = Student("Kate")
+
 print(nick.height)
 print(kate.height)
 nick.printer()
 print(nick.gladness)
-print(nick.gladness)
+print(kate.gladness)
+
+buddy = Pet("Buddy", "dog")
+nick.adopt_pet(buddy)
+nick.play_with_pet()
+kate.play_with_pet()  # У неї немає тварини
 
 
 class Wow:
@@ -37,6 +70,7 @@ class Wow:
 
     def _hello(self):
         print('hello')
+
 
 obj = Wow()
 obj._hello()
@@ -78,13 +112,14 @@ class Parent(GrandParent):
     def about_myself(self):
         print('i am parent')
 
+
 class Child(Parent):
     def __init__(self):
         super().about()
         super().about_myself()
 
-nick = Child()
 
+nick = Child()
 
 
 class Computer:
@@ -106,9 +141,9 @@ class Display:
         print('i display an image')
 
 
-class SmartPhone(Computer, Display):  # Змінили порядок успадкування
+class SmartPhone(Computer, Display):
     def __init__(self, model):
-        super().__init__(model)  # Передаємо model у Computer.__init__()
+        super().__init__(model)
 
     def print_info(self):
         print(self.memory)
@@ -118,4 +153,3 @@ class SmartPhone(Computer, Display):  # Змінили порядок успад
 
 phone = SmartPhone(model='super max pro 10')
 phone.print_info()
-
